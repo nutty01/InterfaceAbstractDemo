42733ab692799d30386f07e083f0474f10f5ccc8
Sena Alzyod <77552412+nutty01@users.noreply.github.com>
2021-02-16 00:27:46 +0300

    Add files via upload

---
+++ Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using MernisServiceReference;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo.Adapters
+{
+    public class MernisServiceAdapter:ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return  client.TCKimlikNoDogrulaAsync(new TCKimlikNoDogrulaRequest
+            (new TCKimlikNoDogrulaRequestBody(Convert.ToInt64(customer.NationalityId),
+                customer.FirstName.ToUpper(),
+                customer.LastName.ToUpper(),customer.DateOfBirth.Year))).Result.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
