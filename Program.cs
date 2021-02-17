aac96358e00b37f0f6a7d3cd784e424b7468ca20
Sena Alzyod <77552412+nutty01@users.noreply.github.com>
2021-02-17 19:25:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer{DateOfBirth = new DateTime(1995,1,6),
+                FirstName = "Sena",LastName = "Alzyod",NationalityId = ""});
+            Console.ReadLine();
+        }
+
+        
+    }
+}
